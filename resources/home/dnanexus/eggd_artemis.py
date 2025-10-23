22d03227d628a2e10702ae4ed70aecf449d65955
spaul91 <sophie.paul@addenbrookes.nhs.uk>
2025-10-23 15:21:20 +0100

    changed from flag-checking to positive-state processing

--- resources/home/dnanexus/eggd_artemis.py
+++ resources/home/dnanexus/eggd_artemis.py
@@ -147,39 +147,36 @@ def main(
 
     multiqc_url = make_url(multiqc_report, project_id, url_duration)
 
-    ## handling NMDs
+    ## NMD reporting
     nmd_samples = []
     nmd_panels = []
-    nmd_output_file = "nmd.csv" # PLEASE HELP ME NAME THIS
+
+    # setting up defaults - in some of the NMD checks, we want to replace missing data with empty data.
+    # We're only concerned with the count in the instance of SNVs
+    default_snv_report = [{"SNV count": 0}]
+    # The CNV report has only two entries that we check: the CNV count, and dataframe of missing regions.
+    # The "default" is a dataframe with only one row - the column names. One of our checks is to look for instances where len(df) <= 1
+    indices = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+    columns = ["CNV excluded regions", "Chrom", "Start", "End", "Length", "Gene_Symbol", "HGNC_ID", "Transcript", "Exon"]
+    excluded_regions = [{index: column} for index, column in zip(indices, columns)]
+    default_cnv_report = [{"CNV count": 0, "cnv_excluded_regions_df": pd.DataFrame(excluded_regions)}]
+
     for sample, sample_data in all_sample_outputs.items():
         panels = sample_data["clinical_indications"]
-        if len(panels) != 1:
-            continue
-        panel, variants = list(panels.items())[0]
-        n_snv_reports = len(variants["SNV"])
-        if "CNV" not in variants:
-            n_cnv_reports = 0
-        else:
-            n_cnv_reports = len(variants["CNV"])
-        if n_snv_reports > 1 or n_cnv_reports > 1:
-            continue
-        elif n_snv_reports == 0 and n_cnv_reports == 0:
-            continue
-        snv_count = int(variants["SNV"][0]["SNV count"])
-        if n_cnv_reports == 1:
-            cnv_count = int(variants["CNV"][0]["CNV count"])
-            cnv_excluded_regions = variants["CNV"][0]["cnv_excluded_regions_df"]
-        else:
-            cnv_count = 0
-            cnv_excluded_regions = None
-        if snv_count > 0 or cnv_count > 0:
-            continue
-        elif cnv_excluded_regions is None:
-            nmd_samples.append(sample)
-            nmd_panels.append(panel)
-        elif len(cnv_excluded_regions) <= 1:
-            nmd_samples.append(sample)
-            nmd_panels.append(panel)
+        if len(panels) == 1:
+            panel, variants = list(panels.items())[0]
+            reports = [variants.get(k, []) for k in ["SNV", "CNV"]]
+            counts = [len(report) for report in reports]
+            # Process the sample if and only if 1 SNV report and/or 1 CNV report exist
+            if counts in [[0, 1], [1, 0], [1, 1]]:
+                snv_report = variants.get("SNV", default_snv_report)[0]
+                cnv_report = variants.get("CNV", default_cnv_report)[0]
+                if int(snv_report["SNV count"]) == 0 and int(cnv_report["CNV count"]) == 0:
+                    if len(cnv_report["cnv_excluded_regions_df"]) <= 1:
+                        nmd_samples.append(sample)
+                        nmd_samples.append(panel)
+
+    nmd_output_file = "nmd.csv"
     if len(nmd_samples) > 0:
         write_nmd_data(nmd_samples, nmd_panels, output_path = nmd_output_file)
     else:
